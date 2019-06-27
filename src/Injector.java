5cfbe578f72043c2c59cb3a6c3d2916176dcdc11
UKiyanitsa <u.kiyanitsa@gmail.com>
2019-06-27 08:57:57 +0300

    first

---
+++ src/Injector.java
@@ -0,0 +1,23 @@
+import java.lang.reflect.Field;
+
+public class Injector {
+    public static void injectDependency() throws IllegalAccessException {
+        Class<ConsoleHandler> consoleHandlerClass = ConsoleHandler.class;
+        Class<BetDaoInterface> betDaoImplClass = BetDaoInterface.class;
+        Class<CustomerDaoInterface> customerDaoClass = CustomerDaoInterface.class;
+
+        Field[] consoleHandlerFields = consoleHandlerClass.getDeclaredFields();
+
+        for (Field field : consoleHandlerFields) {
+            if (field.getDeclaredAnnotation(Inject.class) != null) {
+                field.setAccessible(true);
+                if (field.getType().equals(betDaoImplClass)) {
+                    field.set(null, BetDaoFactory.getInstance());
+                }
+                if (field.getType().equals(customerDaoClass)) {
+                    field.set(null, CustomerDaoFactory.getInstance());
+                }
+            }
+        }
+    }
+}
---
+++ src/Storage.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Storage {
+    public static final List<Bet> BETS = new ArrayList<>();
+    public static final List<Customer> CUSTOMERS = new ArrayList<>();
+}
